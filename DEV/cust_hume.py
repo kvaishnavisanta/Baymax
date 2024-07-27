4c315e82b175c8180d3af46f914c70d2282ed0a9
xOtic | EVIL <58539553+likith18@users.noreply.github.com>
2024-07-27 11:27:50 +0530

    Update cust_hume.py

--- DEV/cust_hume.py
+++ DEV/cust_hume.py
@@ -4,7 +4,7 @@ from hume import HumeVoiceClient, MicrophoneInterface
 
 
 async def main() -> None:
-    client = HumeVoiceClient("k4tzTx8wFZOGgmCKEes1khlzcLK1270ohsbcjKnoptT6FPyp")
+    client = HumeVoiceClient("HUME_API_KEY_HERE")
 
     async with client.connect(config_id="6aa1a7ca-d3d0-4ea9-9e7d-97b064e7c109") as socket:
         await MicrophoneInterface.start(socket)
